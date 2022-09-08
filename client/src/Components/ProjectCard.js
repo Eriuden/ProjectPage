48f2b97e35280ebbdf06555f5de6bef99cd0e566
Eriuden <antoinecarrette@hotmail.fr>
2022-09-08 21:44:06 +0200

    littlecorrections

--- client/src/Components/ProjectCard.js
+++ client/src/Components/ProjectCard.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getProject } from '../redux/actions/project.action'
 import DeleteButton from './DeleteButton'
+import UpdateProjectForm from './UpdateProjectForm'
 
 
 export default function ProjectCard(projectProps) {
 
-  
+  const [updateForm, setUpdateForm] = useState(false)
+
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getProject(project._id))
+  })
+
   return (
     <div>
       <img src={projectProps.picture} alt="" />
@@ -13,6 +23,13 @@ export default function ProjectCard(projectProps) {
       <p>Comppletion:{projectProps.completion}</p>
 
       <DeleteButton/>
+
+      <button onClick={setUpdateForm(true)}>Editer</button>
+
+      {updateForm && (
+        <UpdateProjectForm/>
+        )}
+      
 
     </div>
   )
