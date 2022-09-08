18777437d383e95919ca803fe301ae287fb7f51b
Eriuden <antoinecarrette@hotmail.fr>
2022-09-08 11:59:40 +0200

    front2ndcommit

--- client/src/Pages/UserPage.js
+++ client/src/Pages/UserPage.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUser } from '../redux/actions/user.action'
+
 
 export default function UserPage() {
+
+  const [updateForm, setUpdateForm] = useState(false)
+
+  const userData = useSelector((state) => state.userReducer)
+  const error = useSelector((state) => state.errorReducer.userError)
+  const dispatch = useDispatch
+
+  const handleUpdate = () => {
+    dispatch(updateUser(userData._id, name, password))
+    setUpdateForm(false)
+  }
+
+
   return (
     <div>
-      
+      <h3>Nom</h3>
+      { updateForm === false && (
+        <>
+          <p onClick={() => setUpdateForm(!updateForm)}>{userData.name}</p>
+          <button onClick={() =>setUpdateForm(!updateForm)}>Changer votre nom</button>
+        
+          
+        </>
+      )}
+
+      {
+        updateForm && (
+          <>
+            <input type="text" defaultValue={userData.name} />
+            <button onClick={handleUpdate}> valider la modification</button>
+          </>
+        )
+      }
+
+      <h3>Mot de passe</h3>
+      { updateForm === false && (
+        <>
+        <p onClick={() => setUpdateForm(!updateForm)}>{userData.password}</p>
+          <button onClick={() =>setUpdateForm(!updateForm)}>Changer votre mot de passe</button>
+        </>
+      )}
+
+      {
+        updateForm && (
+          <>
+            <input type="text" defaultValue={userData.password} />
+            <button onClick={handleUpdate}> valider la modification</button>
+          </>
+        )
+      }
+
     </div>
+
   )
 }
--- client/src/Pages/YourProjectsPages.js
+++ client/src/Pages/YourProjectsPages.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import ProjectCard from '../Components/ProjectCard'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { getAllProjects } from '../redux/actions/project.action'
+import { isEmpty } from '../utils'
+
 
 export default function YourProjectsPages() {
+
+  const projects = useSelector((state)=> state.AllProjectsReducer)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getAllProjects())
+  })
+
   return (
     <div>
+
+      <ul>
+
+        {!isEmpty(projects) && projects.map((project) => {
+          <ProjectCard
+          project={projectProps}
+          key={project._id}
+          />
+        })}
+
+      </ul>
       
     </div>
   )
